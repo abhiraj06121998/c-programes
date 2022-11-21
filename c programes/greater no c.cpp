4ebb65f61cef91af2243b016e848fb27be70d19f
abhiraj06121998 <118713960+abhiraj06121998@users.noreply.github.com>
2022-11-21 12:18:32 +0530

    file upload message

---
+++ c programes/greater no c.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("\n enter a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b &&a>c)
+	{
+		printf("\n a is greater");
+	}
+	else if(b>a &&b>c)
+	{
+		printf("\n b is graeter");
+	}
+	else
+	{
+		printf("\n c is greater");
+	}
+	
+	
+	
+}
